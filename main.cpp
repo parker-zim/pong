765e2ad95b3c7917d0e15febe563013b93a9e9ac
parker-zim <pazimmerman@vassar.edu>
2018-09-20 12:26:26 -0400

    added functionality for paddle

--- main.cpp
+++ main.cpp
@@ -68,6 +68,33 @@ public:
   }
 };
 
+class cPaddle{
+private:
+  int x, y;
+  int originalX, originalY;
+public:
+  cPaddle(){
+    x = y = 0;
+  }
+
+  cPaddle(int posX, int posY) : cPaddle() {
+    originalX = posX;
+    originalY = posY;
+    x = posX;
+    y = posY;
+  }
+  inline void Reset() { x = originalX; y = originalY;}
+  inline int getX() {return x;}
+  inline int getY() {return y;}
+  inline void moveUp() {y--;}
+  inline void moveDown() {y++;}
+
+  friend ostream & operator << (ostream & o, cPaddle c){
+    o << "Paddle [" << c.x << "," << c.y << "]";
+    return o;
+  }
+};
+
 
 int main(){
 
